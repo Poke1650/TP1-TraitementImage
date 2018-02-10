297ea28457f2e827fb405325f8caa4e179449d10
LeBeurre <francis.forest121@gmail.com>
2018-02-10 16:12:02 -0500

    Matrice et MatricePixel

--- src/traitement/Main.java
+++ src/traitement/Main.java
@@ -7,6 +7,24 @@ package traitement;
 public class Main {
 
     public static void main(String[] args) {
+        Matrice matrice = new Matrice(7, 5);
+        
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 5; j++) {
+                matrice.setValue(i, j, i * 5 + j);
+            }
+        }
+        
+        System.out.println(matrice.getHeight());
+        System.out.println(matrice.getWidth());
+        matrice.print();
+        
+        matrice.resize(10,10);
+        System.out.println("============");
+        
+        System.out.println(matrice.getHeight());
+        System.out.println(matrice.getWidth());
+        matrice.print();
         
     }
 }
